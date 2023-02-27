eaa34cb78c0bae58c3d6cd34516074fe7928b01d
zltan000 <84435253+zltan000@users.noreply.github.com>
2023-02-27 21:52:12 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as f:
+  long_description = f.read()
+
+setuptools.setup(
+  name="BLiC",
+  version="2023.2.0",
+  author="Zhenlin Tan",
+  author_email="zltan999@sjtu.edu.cn",
+  description="A python package to Build Light-cones and output mock Catalogues",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/zltan000/BLiC",
+  packages=setuptools.find_packages(),
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
